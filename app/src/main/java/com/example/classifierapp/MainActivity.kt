d1a4ab920a059840ed302049116ec48c746d77c3
bzzbzzbee <chikipony666@gmail.com>
2021-08-23 23:15:47 +0700

    CameraX update usage + ML Kit import

--- app/src/main/java/com/example/classifierapp/MainActivity.kt
+++ app/src/main/java/com/example/classifierapp/MainActivity.kt
@@ -1,34 +1,29 @@
 package com.example.classifierapp
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.DisplayMetrics
-import android.util.Rational
-import android.util.Size
-import android.view.Surface
 import android.content.pm.PackageManager
 import android.graphics.*
-import android.os.Handler
-import android.os.HandlerThread
 import android.util.Log
 import androidx.core.content.ContextCompat
 import android.widget.Toast
 import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
-import com.example.classifierapp.data.TFLiteClassifier
 import com.example.classifierapp.databinding.ActivityMainBinding
-import java.io.ByteArrayOutputStream
+import com.google.mlkit.vision.common.InputImage
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
+@androidx.camera.core.ExperimentalGetImage
 class MainActivity : AppCompatActivity() {
 
-
-    private var lensFacing = CameraX.LensFacing.BACK
     private val TAG = "MainActivity"
 
-    private val REQUEST_CODE_PERMISSIONS = 101
-    private val REQUIRED_PERMISSIONS = arrayOf("android.permission.CAMERA")
-
-    private var tfLiteClassifier: TFLiteClassifier = TFLiteClassifier(this@MainActivity)
+    private val REQUEST_CODE_PERMISSIONS = 10
+    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    private lateinit var cameraExecutor: ExecutorService
 
     private lateinit var binding: ActivityMainBinding
 
@@ -39,5 +34,98 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(view)
 
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        requestCameraPermissions()
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val imageCapture = ImageCapture.Builder()
+                .build()
+
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, { imageProxy ->
+                        val mediaImage = imageProxy.image
+                        mediaImage.let {
+                            val image: InputImage = InputImage.fromMediaImage(
+                                mediaImage,
+                                imageProxy.imageInfo.rotationDegrees
+                            )
+                            // Pass image to an ML Kit Vision API
+                        }
+                    })
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture, imageAnalyzer
+                )
+
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT)
+                    .show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestCameraPermissions() {
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 }
--- app/src/main/java/com/example/classifierapp/data/TFLiteClassifier.kt
+++
@@ -1,6 +0,0 @@
-package com.example.classifierapp.data
-
-import android.content.Context
-
-class TFLiteClassifier(private val context: Context) {
-}
